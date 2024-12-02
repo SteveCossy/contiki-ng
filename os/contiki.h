0fc933038bc810c1c9f46ebbaf2c5104b091df78
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2024-12-02 13:00:21 +1300

    2024-12-02_13:00:00

--- os/contiki.h
+++ os/contiki.h
@@ -59,8 +59,7 @@
 
 #endif /* CONTIKI_H_ */
 
-// compile with Contiki-ng Classic - added by Steve a while ago
+// compile with Contiki-ng Classic - added by Steve
 #define ROUTING_CONF_RPL_CLASSIC 1
-/* The following lines don't appear to be necessary 
-#define ROUTING_CONF_RPL_LITE 0 */
+#define uiplib_ipaddr_print(addr)
 
