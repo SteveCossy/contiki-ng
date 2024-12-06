612484671cdf855ae291cf02b32ad4a231ccc2f6
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2024-12-06 18:00:15 +1300

    2024-12-06_18:00:00

--- examples/rpl-border-router/project-conf.h
+++ examples/rpl-border-router/project-conf.h
@@ -80,3 +80,5 @@ Added to create second DODAG
 */
 #define RPL_CONF_MAX_INSTANCES 2   // Allow up to two RPL instances
 #define RPL_CONF_MOP RPL_MOP_NON_STORING   // Set the Mode of Operation
+
+#define Steves_test "project-conf.h"
