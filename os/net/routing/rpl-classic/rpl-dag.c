a400ac86df9bf02f68a181911ab54b2426739ca3
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-01-15 17:40:13 +1300

    2025-01-15_17:40:00

--- os/net/routing/rpl-classic/rpl-dag.c
+++ os/net/routing/rpl-classic/rpl-dag.c
@@ -728,7 +728,7 @@ rpl_alloc_dag(uint8_t instance_id, uip_ipaddr_t *dag_id)
     /* if(dag_id != NULL) {
       uip_ipaddr_copy(&dag->dag_id, &dag_id);
     }*/
-    memcpy(&dag->dag_id, dag_id, sizeof(dag_id));
+    memcpy(dag->dag_id, dag_id, sizeof(dag_id));
 
 
   for(dag = &instance->dag_table[0], end = dag + RPL_MAX_DAG_PER_INSTANCE; dag < end; ++dag) {
