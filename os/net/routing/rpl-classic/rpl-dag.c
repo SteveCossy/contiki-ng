db9a44192eb0e4785872876a397d36e0d3da75da
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-01-30 19:40:27 +1300

    2025-01-30_19:40:00

--- os/net/routing/rpl-classic/rpl-dag.c
+++ os/net/routing/rpl-classic/rpl-dag.c
@@ -515,7 +515,7 @@ rpl_set_root(uint8_t instance_id, uip_ipaddr_t *dag_id) // definition
     return NULL;
 	}
 */
-  //instance = dag->instance;
+  instance = dag->instance;
   //display_dodag(instance);
 
   dag->version = version;
