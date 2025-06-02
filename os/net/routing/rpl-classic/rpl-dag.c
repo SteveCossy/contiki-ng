b379d79ac61148b1cb318fc11c2dafcd2989acbc
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-06-03 11:15:54 +1200

    General Update

--- os/net/routing/rpl-classic/rpl-dag.c
+++ os/net/routing/rpl-classic/rpl-dag.c
@@ -900,6 +900,8 @@ rpl_select_dag(rpl_instance_t *instance, rpl_parent_t *p)
   rpl_rank_t old_rank;
 
   LOG_DBG("Selecting a DAG - rpl-dag.c: 896\n");
+
+  
   old_rank = instance->current_dag->rank;
   last_parent = instance->current_dag->preferred_parent;
 
