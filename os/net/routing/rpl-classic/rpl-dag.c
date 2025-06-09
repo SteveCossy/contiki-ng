43ee59967111160899b5799290223183ab16bbf0
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-06-09 15:00:13 +1200

    2025-06-09_15:00:00

--- os/net/routing/rpl-classic/rpl-dag.c
+++ os/net/routing/rpl-classic/rpl-dag.c
@@ -503,7 +503,7 @@ rpl_set_root(uint8_t instance_id, uip_ipaddr_t *dag_id) // definition
 	  LOG_DBG("DAG allocated with ID: ");
 		LOG_DBG_6ADDR(dag_id);
 	  LOG_DBG(", Prefix: ");
-    LOG_DBG("%u", (unsigned int)dag_id->u16)
+    LOG_DBG(" %"PRIu16" ", dag_id->u16)
     LOG_DBG_("\n");
 	} else {
 		// DAG allocation failed
