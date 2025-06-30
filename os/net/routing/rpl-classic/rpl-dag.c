6623c7d139d43ce7a9ff1da9e2b96aed950a0a00
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-07-01 11:20:15 +1200

    THW-2025-07-01_11:20:01

--- os/net/routing/rpl-classic/rpl-dag.c
+++ os/net/routing/rpl-classic/rpl-dag.c
@@ -618,11 +618,11 @@ rpl_set_root(uint8_t instance_id, uip_ipaddr_t *dag_id) // definition
     LOG_DBG_(" One ");
     /* Debug - use OF0 for both DODAGs
     instance->of = rpl_find_of(RPL_OF_OCP); */
-    instance->of = rpl_find_of(RPL_OCP_OF0);
+    instance->of = rpl_find_of(RPL_OCP_MRHOF);
   }
   else {
     LOG_DBG_(" Two ");
-    instance->of = rpl_find_of(RPL_OCP_OF0);
+    instance->of = rpl_find_of(RPL_OCP_MRHOF);
   }
   LOG_DBG_("\n");
 
