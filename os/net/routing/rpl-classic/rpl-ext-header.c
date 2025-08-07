d8e809aa5051dbe01b8fc328299fd6628f857cba
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-08-08 09:20:17 +1200

    THW-2025-08-08_09:20:00

--- os/net/routing/rpl-classic/rpl-ext-header.c
+++ os/net/routing/rpl-classic/rpl-ext-header.c
@@ -58,7 +58,7 @@
 #include <limits.h>
 #include <string.h>
 
-static rpl_instance_t *current_instance_for_hbh = NULL;
+// 0807 static rpl_instance_t *current_instance_for_hbh = NULL;
 
 /**
  * \brief Finds an active RPL instance that matches a given IPv6 address prefix.
@@ -664,23 +664,26 @@ rpl_ext_header_remove(void)
 int
 rpl_ext_header_update(void)
 {
-  rpl_instance_t *instance = NULL
+  rpl_instance_t *instance = NULL;
   const uip_ip6addr_t *dest_addr;
 
   // Get the destination address from the global packet buffer.
   // The destination address is in the main IPv6 header.
-  dest_addr = &UIP_IP_BUF->destipaddr;
+  dest_addr = &UIP_IP_BUF->srcipaddr; // 0807 
+  // Find the current RPL instance for the destination prefix
+  instance = rpl_get_instance_from_prefix(dest_addr); // 0807 
 
-  // Try to get the instance from the packet buffer attributes.
-  // This is the most reliable method for locally generated control packets like DIOs.
-  instance = (rpl_instance_t *)packetbuf_addr(PACKETBUF_ADDR_RPL_INSTANCE);
+  // 0807 
+  // // Try to get the instance from the packet buffer attributes.
+  // // This is the most reliable method for locally generated control packets like DIOs.
+  // instance = (rpl_instance_t *)packetbuf_addr(PACKETBUF_ADDR_RPL_INSTANCE);
 
-  // If the instance was not in the packet buffer, fall back to the prefix method.
-  // This will handle forwarded unicast data packets.
-  if(instance == NULL) {
-    dest_addr = &UIP_IP_BUF->destipaddr;
-    instance = rpl_get_instance_from_prefix(dest_addr);
-  }
+  // // If the instance was not in the packet buffer, fall back to the prefix method.
+  // // This will handle forwarded unicast data packets.
+  // if(instance == NULL) {
+  //   dest_addr = &UIP_IP_BUF->destipaddr;
+  //   instance = rpl_get_instance_from_prefix(dest_addr);
+  // }
 
     // Check if we found a valid instance for this packet.
   if(instance == NULL || instance->current_dag == NULL) {
