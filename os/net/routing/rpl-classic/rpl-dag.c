325a6829ae336c18331276e154d50da1c08f149c
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-08-20 13:00:18 +1200

    THW-2025-08-20_13:00:00

--- os/net/routing/rpl-classic/rpl-dag.c
+++ os/net/routing/rpl-classic/rpl-dag.c
@@ -1556,9 +1556,9 @@ rpl_join_instance(uip_ipaddr_t *from, rpl_dio_t *dio)
   /* So far this is the lowest rank we are aware of. */
   dag->min_rank = dag->rank;
 
-  if(default_instance == NULL) {
-    default_instance = instance;
-  } // lost one DODAG when this was commented out
+ // if(default_instance == NULL) {
+//    default_instance = instance;
+//  } // lost one DODAG when this was commented out
 
   LOG_INFO("Joined DAG with instance ID %u, rank %hu, DAG ID ",
            dio->instance_id, dag->rank);
