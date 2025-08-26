97dc0e83449f97a4a378600c185adc0c74e6f42c
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-08-26 14:20:20 +1200

    THW-2025-08-26_14:20:00

--- os/net/routing/rpl-classic/rpl-icmp6.c
+++ os/net/routing/rpl-classic/rpl-icmp6.c
@@ -1310,6 +1310,7 @@ dao_output_target_seq(rpl_parent_t *parent, uip_ipaddr_t *prefix,
   int pos;
   uip_ipaddr_t *parent_ipaddr = NULL;
   uip_ipaddr_t *dest_ipaddr = NULL;
+  RPL_DEBUG_DAO_OUTPUT = 1;
 
   /* Destination Advertisement Object */
 
