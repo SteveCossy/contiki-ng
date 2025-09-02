2b4f712708d8b6744cbf42799128e499a961b942
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-09-02 17:00:20 +1200

    THW-2025-09-02_17:00:00

--- os/net/routing/rpl-classic/rpl-dag.c
+++ os/net/routing/rpl-classic/rpl-dag.c
@@ -433,8 +433,7 @@ rpl_set_preferred_parent(rpl_dag_t *dag, rpl_parent_t *p)
   LOG_INFO("rpl_set_preferred_parent, was:");
   if(dag->preferred_parent != NULL) {
     LOG_INFO_6ADDR(rpl_parent_get_ipaddr(dag->preferred_parent));
-    LOG_INFO_(" Instance %u.\n                 rpl_set DODAGID:",
-      dag->instance->instance_id);
+    LOG_INFO_(" Instance %u.\n                 rpl_set DODAGID:",dag->instance->instance_id);
     LOG_INFO_6ADDR(&dag->dag_id);
   } else {
     LOG_INFO_("NULL");
@@ -447,7 +446,7 @@ rpl_set_preferred_parent(rpl_dag_t *dag, rpl_parent_t *p)
   }
   LOG_INFO_(".\n");
 
-  if(dag == NULL || dag->preferred_parent == p || p == NULL) {
+  if(dag == NULL || dag->preferred_parent == p) {
     return;
   }
 
