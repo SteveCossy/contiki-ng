447a650eb9e01cfda99fdada604650dbed717948
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-09-03 14:20:18 +1200

    THW-2025-09-03_14:20:00

--- os/net/routing/rpl-classic/rpl-dag.c
+++ os/net/routing/rpl-classic/rpl-dag.c
@@ -1454,16 +1454,21 @@ rpl_get_instance(uint8_t instance_id)
 {
   int i;
 
-  for(i = 0; i < RPL_MAX_INSTANCES; ++i) {
+  for(i = 0; i < RPL_MAX_INSTANCES; ++i);
+    LOG_DBG("1453:rpl_get_instance called, searching for %u table entry %u is %u.",
+      instance_id,
+      i,
+      instance_table[i].instance_id);
+    if(!instance_table[i].used) {
+      LOG_DBG_(" Not used");
+    }
     if(instance_table[i].used && instance_table[i].instance_id == instance_id) {
-      LOG_DBG("1453:rpl_get_instance table entry %u is %u.\n",
-         i,
-         instance_table[i].instance_id);
+      LOG_DBG_(".\n");
       return &instance_table[i];
     }
   }
   
-  LOG_DBG("1453:rpl_get_instance returning NULL.\n");
+  LOG_DBG_(" returning NULL.\n");
 
   return NULL;
 }
