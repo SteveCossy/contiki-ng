19a38b12f0682f9dd16304110409727ece176a86
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-09-09 17:20:20 +1200

    THW-2025-09-09_17:20:00

--- os/net/routing/rpl-classic/rpl-dag.c
+++ os/net/routing/rpl-classic/rpl-dag.c
@@ -433,7 +433,7 @@ rpl_set_preferred_parent(rpl_dag_t *dag, rpl_parent_t *p)
   LOG_INFO("rpl_set_preferred_parent, was:");
   if(dag->preferred_parent != NULL) {
     LOG_INFO_6ADDR(rpl_parent_get_ipaddr(dag->preferred_parent));
-    LOG_INFO_(" Instance %u.\n                 rpl_set DODAGID:",dag->instance->instance_id);
+    LOG_INFO_(" Instance %u, rpl_set DODAGID: ",dag->instance->instance_id);
     LOG_INFO_6ADDR(&dag->dag_id);
   } else {
     LOG_INFO_("NULL");
@@ -1485,18 +1485,20 @@ rpl_get_instance(uint8_t instance_id)
   int i;
 
   for(i = 0; i < RPL_MAX_INSTANCES; ++i) {
-    LOG_DBG("1458:rpl_get_instance called, target %u table entry %u is %u",
+    LOG_DBG("rpl_get_instance called, target %u table entry %u is %u",
       instance_id,
       i,
       instance_table[i].instance_id);
     if(!instance_table[i].used) {
-      LOG_DBG_(". Not used");
+      LOG_DBG_(". Not used.\n");
     } 
+    if(!instance_table[i].instance_id == instance_id){
+      LOG_DBG_(".\n");
+    }
     if(instance_table[i].used && instance_table[i].instance_id == instance_id) {
       LOG_DBG_(" returning.\n");
       return &instance_table[i];
     }
-    LOG_DBG_(".\n");
   }
   
   LOG_DBG_(" returning NULL.\n");
