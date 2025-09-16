a7333de94dcffa314278d2d41b116a5b21d3e633
Steve Cosgrove <stevecos@ecs.vuw.ac.nz>
2025-09-17 10:00:19 +1200

    THW-2025-09-17_10:00:01

--- os/net/routing/rpl-classic/rpl-conf.h
+++ os/net/routing/rpl-classic/rpl-conf.h
@@ -51,11 +51,11 @@
 */
 
 // Non-storing routing mode is
-// #define RPL_CONF_MOP RPL_MOP_NON_STORING
+ #define RPL_CONF_MOP RPL_MOP_NON_STORING
 // Storing routing mode is (testing)
 // #define RPL_CONF_MOP RPL_MOP_STORING_MULTICAST
 // There is also
- #define RPL_CONF_MOP RPL_MOP_STORING_NO_MULTICAST
+// #define RPL_CONF_MOP RPL_MOP_STORING_NO_MULTICAST
 
 /* DAG Mode of Operation */
 #define RPL_MOP_NO_DOWNWARD_ROUTES      0
